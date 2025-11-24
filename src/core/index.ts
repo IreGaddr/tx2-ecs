89356445aac7de5c94af611a4a767051a5453113
IreGaddr <iregaddr@gmail.com>
2025-11-24 11:00:54 -0600

    Fix type-only exports and release 0.1.6

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,37 +1,39 @@
-export { Entity, EntityId, createEntityId, resetEntityIdCounter } from './entity.js';
+export { Entity, createEntityId, resetEntityIdCounter } from './entity.js';
+export type { EntityId } from './entity.js';
+
 export {
   Component,
-  ComponentId,
-  ComponentType,
-  ComponentClass,
-  ComponentData,
   ComponentStore,
   defineComponent,
   getComponentClass,
   getComponentId,
   getAllComponents,
 } from './component.js';
-export {
-  Query,
-  QueryBuilder,
-  QueryCache,
-  QueryDescriptor,
-  QueryFilter,
-} from './query.js';
+export type { ComponentId, ComponentType, ComponentClass, ComponentData } from './component.js';
+
+export { Query, QueryBuilder, QueryCache } from './query.js';
+export type { QueryDescriptor, QueryFilter } from './query.js';
+
 export {
   System,
-  SystemId,
-  SystemPhase,
-  SystemContext,
-  SystemFunction,
-  SystemDescriptor,
   SystemScheduler,
   defineSystem,
   createSystemId,
   ThreadedSystem,
 } from './system.js';
+export type {
+  SystemId,
+  SystemPhase,
+  SystemContext,
+  SystemFunction,
+  SystemDescriptor,
+} from './system.js';
+
 export { Logger, LogLevel, PerformanceMetrics, logger } from './logger.js';
-export { createWorker, IsomorphicWorker, IWorker, WorkerMessage } from './worker.js';
+
+export { createWorker, IsomorphicWorker } from './worker.js';
+export type { IWorker, WorkerMessage } from './worker.js';
+
 export {
   TX2Error,
   SystemErrorStrategy,
@@ -39,4 +41,6 @@ export {
   SystemErrorHandler,
   defaultErrorHandler,
 } from './error.js';
-export { World, WorldConfig } from './world.js';
+
+export { World } from './world.js';
+export type { WorldConfig } from './world.js';
